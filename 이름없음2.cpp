dd551d5aa4f94fd8d3d6dc2cd7738972b506b16d
gaaallp <wk2009ascilaz@gmail.com>
2025-08-06 16:18:26 +0900

    Add files via upload

---
+++ 이름없음2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	  int num1;
+	  int num2, num3;
+	  char ch;
+	  getchar();
+	  return 0;
+}
---
+++ 이름없음3.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int num1 = 10;
+	int num2, num3;
+	num2 = 20;
+	num3 = 30;
+	char ch;
+	ch = 'A';
+	
+	
+	printf("num1 값: %d \n", num1);
+	printf("num2 값: %d, num3 값: %d \n", num2, num3);
+	printf("ch값: %c \n", ch);
+	
+	
+	getchar();
+	return 0;
+	
+		
+}
